ffadf71293fee791e4017dcad4280caf42fd8cc7
Clint Shryock <clint@ctshryock.com>
2011-04-13 20:23:12 -0500

    Added some documentation for the `create_site` file.
    lib/jekyll/create_site.rb contains all the logic to setup a new
    bare-bones Jekyll site by creating:
    
    - A directory specified by the "--init" command line option
    - An index.md file
    - A _config.yml file
    - A _layouts folder
      - A default.html layout
    - A _posts folder
      - An example first post

--- lib/jekyll/create_site.rb
+++ lib/jekyll/create_site.rb
@@ -1,6 +1,10 @@
 module Jekyll
 	class CreateSite
 
+    # Initialize a new Jekyll site
+    #   +name+ is the new Jekyll site folder name
+    #
+
 		def initialize(name)
       @site_name = name
 			FileUtils.mkdir(@site_name)
@@ -12,11 +16,20 @@ module Jekyll
       first_post
 		end
 
+    # Create the required subdirectories
+    #   _layouts
+    #   _posts
+    #
+    # Returns true if successfully created _posts
     def sub_directories
 			FileUtils.mkdir_p("_layouts")
 			FileUtils.mkdir_p("_posts")
     end
 
+    # Create a starter config file
+    # 
+    # Jekyll has auto:false as a default already, but I needed to put something here
+    # as an example
     def config_file
 			File.open("_config.yml", 'w') do |f| 
 				# this is alread a default, but need to write something
@@ -25,31 +38,11 @@ module Jekyll
 			end
     end
 
-    def index_page
-			File.open("index.md", 'w') do |f|
-				f.write <<EOF
----
-layout: default
----
 
-###{@site_name}
-
-Welcome to your new Jekyll site
-
-<ul>
-    {% for post in site.posts %}
-    <li><a href="{{post.url}}" title="{{post.title}}">{{post.title}}</a></li>
-    {% endfor %}
-</ul>
-EOF
-				f.close
-			end
-    end
-
-    def formatted_date
-      Time.now.strftime("%F")
-    end
-
+    # Create the default_layout file
+    #
+    # Displays a page's title in the <title/> if page exists
+    # Displays content
     def default_layout 
       File.open("_layouts/default.html", 'w') do |f|
 				f.write <<EOF
@@ -72,6 +65,31 @@ EOF
 			end
     end
 
+    # Create the index.md file
+    # Uses default layout
+    # Lists all posts 
+    def index_page
+			File.open("index.md", 'w') do |f|
+				f.write <<EOF
+---
+layout: default
+---
+
+###{@site_name}
+
+Welcome to your new Jekyll site
+
+<ul>
+    {% for post in site.posts %}
+    <li><a href="{{post.url}}" title="{{post.title}}">{{post.title}}</a></li>
+    {% endfor %}
+</ul>
+EOF
+				f.close
+			end
+    end
+
+    # Create a first post in _posts with today's date 
     def first_post
 			File.open("_posts/#{formatted_date}-first_post.md", 'w') do |f|
 				f.write <<EOF
@@ -83,6 +101,12 @@ First post!
 EOF
 				f.close
 			end
+    end
+    
+    # Returns a formatted date string
+    # Used in creating first post
+    def formatted_date
+      Time.now.strftime("%F")
     end
 
 	end
